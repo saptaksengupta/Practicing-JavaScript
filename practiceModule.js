428ea308cd6dfc0cf05f888b1d02518e2ee7a1d5
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-01-05 11:43:27 +0530

    Dom Bindings Added

--- practiceModule.js
+++ practiceModule.js
@@ -1,7 +1,9 @@
 'use strict'
 
 var requiredDomBindings = {
-    searchBox: document.getElementById('search-box')
+    searchBox: document.getElementById('search-box'),
+    nameInput: document.getElementById('name'),
+    nameContainer: document.getElementById('name-container')
 }
 
 var SomeServiceModule = (function(){
@@ -17,9 +19,21 @@ var SomeServiceModule = (function(){
 
 var SomeModule = (function (bindedDoms, someService){
 
+    function _init(){
+        _attachListeners();    
+    }
+
+    function _attachListeners(){
+        bindedDoms.nameInput.addEventListener('keyup', onNameChanged);
+    }
+
     function _somePrivateMethod() {
         console.log('In Private Method');
     };
+
+    var onNameChanged = function(){
+        bindedDoms.nameContainer.innerHTML = bindedDoms.nameInput.value;
+    }
 
     var sumPublicMethod = function ( ){
         _somePrivateMethod();
@@ -30,10 +44,11 @@ var SomeModule = (function (bindedDoms, someService){
 
     //Module Revealing
     return {
+        init: _init,
         publicMethod: sumPublicMethod
     }
 
 }(requiredDomBindings, SomeServiceModule));
 
 
-SomeModule.publicMethod();
+SomeModule.init();
